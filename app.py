f0a2900dd981215123ab5f8babffa1b0c573f1fb
Ahmed <elrazia@users.noreply.github.com>
2020-12-07 08:56:39 -0600

    rewrite minus predict middleware

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 import uvicorn
 
 app = FastAPI()
 
-@app.get('/index')
+@app.get('/')
 def hello_world():
-    return "Hello World!"
+    return "Welcome to the Helmet Predictor API"
+
+@app.post('/api')
+async def image(image: UploadFile = File(...)):
+    return {"filename": image.filename}
 
 if __name__ == "__main__" :
     uvicorn.run(app, port=8080, host='0.0.0.0')
